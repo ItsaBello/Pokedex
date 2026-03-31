acd99294728f43993909f2a6974898a425e008b5
Isabel te Pas <isabeltepas@hotmail.com>
2026-03-31 14:46:54 +0200

    Eerste drie Pokemon toegevoegd

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,19 @@
+type PokemonCardProps = {
+    name: string
+    number: number
+    image: string
+}
+
+function PokemonCard(props: PokemonCardProps) {
+    return (
+        <section>
+            <div>
+                <h2>{props.name}</h2>
+                <p>#{props.number.toString().padStart(4, "0")}</p>
+                <img src={props.image} alt={props.name} />
+            </div>
+        </section>
+    )
+}
+
+export default PokemonCard;
